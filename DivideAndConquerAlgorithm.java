85f6f8a9b92b243da172c989ff4ba996ceef3515
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-02-26 11:53:13 +0000

    Created template

--- DivideAndConquerAlgorithm.java
+++ DivideAndConquerAlgorithm.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 public interface DivideAndConquerAlgorithm<T> {
-  T solve(T problem);
-  String getRecurrence();
+  public abstract Boolean isSmall(T data);
+  public abstract T solveSmall(T data);
+  public abstract ArrayList<T> decompose(T data, Integer size);
+  public abstract T combine(ArrayList<T> solutions);
+  public default T solve(T data, Integer size) {
+    if (isSmall(data)) {
+      return solveSmall(data);
+    } else {
+      ArrayList<T> subproblems = decompose(data, size);
+      ArrayList<T> solutions = new ArrayList<T>();
+      for (T subproblem : subproblems) {
+        solutions.add(solve(subproblem, size));
+      }
+      return combine(solutions);
+    }
+  }
 }
