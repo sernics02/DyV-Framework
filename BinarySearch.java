d9c20f3eaf14c4105cc9e63bdcb69b2f25abf2d7
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-03-05 12:01:59 +0000

    Fichero function added

--- BinarySearch.java
+++
@@ -1,7 +0,0 @@
-public class BinarySearch<T extends Comparable<T>> implements DivideAndConquerAlgorithm<T> {
-  public BinarySearch(T target) {
-    this.target = target;
-  }
-  
-  private T target;
-}
--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
@@ -17,16 +19,31 @@ public class Main {
     endTime = System.currentTimeMillis();
     long quickTime = endTime - startTime;
     generateTableResults(mergeTime, quickTime);
+    toFile(mergeSorted, "solutions.txt");
+    // toFile(quickSorted, "solutions.txt");
   }
   public static ArrayList<Integer> generateArray(Integer size) {
     ArrayList<Integer> data = new ArrayList<Integer>();
     for (int i = 0; i < size; i++) {
-      data.add((int) (Math.random() * 100));
+      data.add((int) (Math.random() * size));
     }
     return data;
   }
   public static void generateTableResults(long mergeTime, long quickTime) {
     System.out.println("Merge Sort Time: " + mergeTime + "ms");
     System.out.println("Quick Sort Time: " + quickTime + "ms");
+  }
+  public static void toFile(ArrayList<Integer> data, String filename) {
+    try {
+      FileWriter file = new FileWriter(filename);
+      for (Integer number : data) {
+        file.write(number + " ");
+      }
+      file.write("\n");
+      file.close();
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
   }
 }
