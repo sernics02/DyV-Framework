64d260c578803ee790d1e853a724de47a181e856
Sergio Nicolas Segui <sergionicolassegui@MacBook-de-Sergio.local>
2024-03-11 08:35:07 +0000

    Intefaz strassen

--- Strassen.java
+++ Strassen.java
@@ -1,3 +1,24 @@
-public class Strassen extends DivideAndConquerAlgorithm<Matrix, Matrix>{
-  
+import java.util.ArrayList;
+
+public class Strassen extends DivideAndConquerAlgorithm<TwoMatrix, Matrix>{
+  @Override
+  public Boolean isSmall(TwoMatrix data) {
+    return data.getA().getRowCount() <= 2;
+  }
+  @Override
+  public Matrix solveSmall(TwoMatrix data) {
+    return null;
+  }
+  @Override
+  public ArrayList<TwoMatrix> decompose(TwoMatrix data, Integer size) {
+    return null;
+  }
+  @Override
+  public Matrix combine(ArrayList<Matrix> solutions) {
+    return null;
+  }
+  @Override
+  public String getRecurrence() {
+    return null;
+  }
 }
